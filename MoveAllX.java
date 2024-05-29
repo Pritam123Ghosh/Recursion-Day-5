43319d020a112224847ca1b3f53049ca351f0770
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-05-29 20:32:03 +0530

    2 Questions

---
+++ MoveAllX.java
@@ -0,0 +1,26 @@
+//Move all X to the end of the string
+//axbcxxd
+public class MoveAllX {
+    public static void move(String str, int idx, int count, String newString) {
+        if (idx == str.length()) {
+            for (int i = 0; i < count; i++) {
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (currChar == 'x') {
+            count++;
+            move(str, idx + 1, count, newString);
+        } else {
+            newString += currChar;
+            move(str, idx + 1, count, newString);
+        }
+    }
+
+    public static void main(String args[]) {
+        String str = "axbcxxd";
+        move(str, 0, 0, "");
+    }
+}
